0b60c45d762d8f032ece7a4e2c61c28e69885edf
Александр Дрожкин <Santay2009@yandex.ru>
2020-11-12 03:21:38 +0300

    Add lib "ParserTxt"

---
+++ ParserTxt/Models/OneRowTxt.cs
@@ -0,0 +1,66 @@
+﻿namespace Bank.Models
+{
+
+	public class OneRowTxt
+	{
+		public OneRowTxt(long x1A, long x1B, long x1V, long x1G, long x2, long x3,
+			long x4, long x5, long x6, long x7, long x8, long x9,
+			long x10, long x11, long x12, long x13, long x14)
+		{
+			X1A = x1A;
+			X1B = x1B;
+			X1V = x1V;
+			X1G = x1G;
+			X2 = x2;
+			X3 = x3;
+			X4 = x4;
+			X5 = x5;
+			X6 = x6;
+			X7 = x7;
+			X8 = x8;
+			X9 = x9;
+			X10 = x10;
+			X11 = x11;
+			X12 = x12;
+			X13 = x13;
+			X14 = x14;
+		}
+
+		/// <summary>
+		/// 1a
+		/// </summary>
+		public long X1A { get; }
+
+		/// <summary>
+		/// 1б
+		/// </summary>
+		public long X1B { get; }
+
+		/// <summary>
+		/// 1в
+		/// </summary>
+		public long X1V { get; }
+
+		/// <summary>
+		/// 1г
+		/// </summary>
+		public long X1G { get; }
+
+		public long X2 { get; }
+		public long X3 { get; }
+		public long X4 { get; }
+		public long X5 { get; }
+		public long X6 { get; }
+		public long X7 { get; }
+		public long X8 { get; }
+		public long X9 { get; }
+		public long X10 { get; }
+		public long X11 { get; }
+		public long X12 { get; }
+		public long X13 { get; }
+		public long X14 { get; }
+
+
+	}
+
+}
---
+++ ParserTxt/ParserTxt.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ParserTxt
+{
+    public sealed class ParserTxt  
+    {
+    }
+}
