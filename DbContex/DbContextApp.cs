c9a8910452132a6e31f6ca124d3cf23661fbb7e8
Александр Дрожкин <Santay2009@yandex.ru>
2020-11-12 17:39:43 +0300

    Add Entity Framework

---
+++ DbContex/DbContextApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using DbContex.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace DbContex
+{
+    public sealed class DbContextApp : DbContext
+    {
+	    public DbContextApp()
+	    {
+		    Database.EnsureCreated();
+	    }
+
+	    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	    {
+		    optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=123123");
+	    }
+        public DbSet<TableFirst> TableFirsts { get; set; }
+    }
+}
---
+++ UnitTest/UnitTest_DbContex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+using DbContex;
+using DbContex.Models;
+
+using NUnit.Framework;
+
+namespace UnitTest
+{
+	[TestFixture]
+	public sealed class UnitTest_DbContex
+	{
+		[Test]
+		public void TestDbContex()
+		{
+			using (DbContextApp db = new DbContextApp())
+			{
+				TableFirst tableFirst1 = new TableFirst(){A1 = 6000};
+				TableFirst tableFirst2 = new TableFirst(){A1 = 99999};
+
+				db.TableFirsts.AddRange(tableFirst1,tableFirst2);
+				db.SaveChanges();
+
+				var table = db.TableFirsts.ToList();
+				table.ForEach(item=>Console.WriteLine(item.A1+Environment.NewLine));
+
+				db.TableFirsts.RemoveRange(db.TableFirsts.Select(item=>item).ToArray());
+				db.SaveChanges();
+
+				if(!db.TableFirsts.Any())
+					Console.WriteLine("База пуста");
+			}
+		}
+	}
+
+}
